513f0579637eb40ba91ed4d785d65d14e11741c1
medc1610 <medc1610@gmail.com>
2021-11-30 00:16:17 -0300

    Guardar

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,21 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent {
+
+  heroes:string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor'];
+  heroeBorrado:string = '';
+
+  borrarHeroe(){
+    console.log('borrando...');   
+    
+    this.heroeBorrado = this.heroes.shift() || '';
+    
+  }
+
+}
